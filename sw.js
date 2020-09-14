fdb61e373e3aaec5e02496a309df8ac8a24585e6
san-men <66144889+san-men@users.noreply.github.com>
2020-09-14 01:28:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,14 +3,9 @@
 const CACHE_NAME = 'v1_cache_programador_fitness',
   urlsToCache = [
     './',
-    'https://fonts.googleapis.com/css?family=Raleway:400,700',
-    'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
-    'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
-    'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
     './style.css',
     './script.js',
-    './img/ProgramadorFitness.png',
-    './img/favicon.png'
+    './img/logo.png'
   ]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
